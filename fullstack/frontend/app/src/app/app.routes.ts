52685c397da553a711ed7ef4d41f5f7c79990c44
Sarah Lengel <sarahnlengel@gmail.com>
2023-07-18 09:43:37 -0400

    done with monday

---
+++ fullstack/frontend/app/src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard.component';
+import { AboutComponent } from './pages/about.component';
+
+
+export const routes: Routes = [
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+
+        path: '**',
+        redirectTo: 'dashboard'
+    }
+];
