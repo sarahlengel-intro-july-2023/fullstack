590576bfbe5384754ea9d27913b484d94415c693
Sarah Lengel <sarahnlengel@gmail.com>
2023-07-24 16:42:00 -0400

    done with monday week 2

---
+++ backend/TodoSolution/TodosApi/Controllers/TodoListController.cs
@@ -0,0 +1,33 @@
+﻿
+
+using Marten;
+
+namespace TodosApi.Controllers;
+
+[ApiController]
+public class TodoListController : ControllerBase
+{
+
+    private readonly IManageTheTodolistCatalog _todoListCatalog;
+
+    public TodoListController(IManageTheTodolistCatalog todoListCatalog)
+    {
+        _todoListCatalog = todoListCatalog;
+    }
+
+    [HttpPost("/todo-list")]
+    public async Task<ActionResult> AddTodoItem([FromBody] TodoListCreateModel request)
+    {
+        var response = await _todoListCatalog.AddTodoItemAsync(request);
+        return Ok(response);
+    }
+
+
+    // GET /todo-list
+    [HttpGet("/todo-list")]
+    public async Task<ActionResult> GetTodoList()
+    {
+        var list = await _todoListCatalog.GetFullListAsync();
+        return Ok(list);
+    }
+}
--- frontend/app/tailwind.config.js
+++ frontend/app/tailwind.config.js
@@ -15,3 +15,4 @@ module.exports = {
     themes: ["aqua", "cupcake", "cyberpunk"]
   }
 }
+
